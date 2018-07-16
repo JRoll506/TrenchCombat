6789362d033527f607a7a6752c4b53f78adbf5c9
Ryan Bruno <ryanbruno506@gmail.com>
2018-07-16 16:17:03 -0400

    Fixed respawn potions

--- plugin/src/main/java/com/rbruno/trench/listener/listeners/PlayerRespawn.java
+++ plugin/src/main/java/com/rbruno/trench/listener/listeners/PlayerRespawn.java
@@ -1,11 +1,13 @@
 package com.rbruno.trench.listener.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 import com.rbruno.trench.Main;
+import com.rbruno.trench.classes.EngineClass;
 import com.rbruno.trench.listener.EngineListner;
 import com.rbruno.trench.timer.GameState;
 
@@ -18,11 +20,22 @@ public class PlayerRespawn extends EngineListner implements Listener {
 	
 	@EventHandler
 	public void onPlayerRespawnEvent(PlayerRespawnEvent event) {
-		Player player = (Player) event.getPlayer();
+		final Player player = (Player) event.getPlayer();
 		if (main.getGameState() == GameState.IN_GAME) {
 			// Send to player's team spawn
 			event.setRespawnLocation(main.game.getColorTeam(player).spawn);
 			main.game.giveItems(player);
+			
+			Bukkit.getScheduler().scheduleSyncDelayedTask(main, new Runnable() {
+				
+				@Override
+				public void run() {
+					EngineClass playerClass = main.classManager.classMap.get(player);
+					if (playerClass.getEffect() != null) {
+						player.addPotionEffect(playerClass.getEffect());
+					}
+				}
+			});
 		} else {
 			// Send to lobby spawn
 			event.setRespawnLocation(main.trenchConfig.getSpawn());
